a280d14fdfef476a2bdf656e9c32747684bb56fc
likedemian <likedemian@me.com>
2017-10-08 22:37:40 +0900

    [ADD] NEW Cine-Kids Files

---
+++ Cine-Kids/dist/js/carousel.js
@@ -0,0 +1,9 @@
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 30,
+        freeMode: true
+    });
+
+
