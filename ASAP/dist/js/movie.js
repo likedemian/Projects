856038fda442687ed8ed4c082f1be0b328f16521
likedemian <likedemian@me.com>
2017-11-01 13:55:39 +0900

    [ADD] Movie Blank Poster if there is nothing to put on the list box

--- ASAP/dist/js/movie.js
+++ ASAP/dist/js/movie.js
@@ -1,17 +1,24 @@
 ;
 (function(global, $) {
   'use strict';
+  var page = 1;
   var movieWrap;
   var movieCoverWrap;
-  var data = [];
 
   function init() {
     movieWrap = $('.main__movie__wrap');
     movieCoverWrap = $('.swiper-wrapper');
-    getMovies();
+    getMovies()
+    bind();
   }
 
-
+  function bind() {
+    $(window).scroll(function() {
+      if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+        ++page && getMovies()
+      }
+    });
+  };
 
   function getMovies() {
     var API = '?api_key=64391ca210dbae0d44b0a622177ef8d3';
@@ -20,17 +27,26 @@
       movies: 'https://api.themoviedb.org/3/movie/',
       search: 'https://api.themoviedb.org/3/search/movie',
       poster_small: 'https://image.tmdb.org/t/p/w342',
-      backdrop : "https://image.tmdb.org/t/p/w1280",  
+      backdrop: "https://image.tmdb.org/t/p/w1280",
       profile: 'https://image.tmdb.org/t/p/w185',
       popularity: '&sort_by=popularity.desc',
-      popularity_recent: '&primary_release_year=2010&sort_by=popularity.desc',
+      popularity_recent: '&primary_release_year=2017&sort_by=popularity.desc',
       korean: '&language=ko',
-      keyword: ''
+      pages: '&page=',
+      keyword: '',
+      search_data: '',
+      noposter: ''
     }
-
-    // state.url_search + state.api_key + state.url_korean + "&query=" + keyword
-    $.get(state.discover + API + state.korean + state.popularity_recent)
+    $.get(state.discover + API + state.popularity_recent + state.pages + page)
       .then((response) => {
+        state.search_data = response.results;
+        let search_length = state.search_data.length;
+        console.log(search_length);
+        console.log(state.search_data.poster_path);
+        for (let i = 0; i < search_length; i++) {
+          (state.search_data.poster_path) ? state.search_data.poster_path = state.url_poster_small + state.search_data.poster_path : state.search_data.poster_path = state.url_noposter_search;
+        }
+
         console.log(response);
         let movies = response.results;
         let coverOutput = '';
@@ -55,14 +71,15 @@
                 <div class="movie__info__wrap">
                   <h3 class="movie__title">${movie.title}</h3>
                   <p class="movie__genre">${movie.genre_ids}</p>
-                <div class="detail__button">
-                  <p class="btn__param">More Info</p>
-                </div>
+                <button class="detail__button">
+                  <p class="ripple btn__param">More Info</p>
+                </button>
                 </div>
               </div>
             </li>
           `;
         });
+
         movieWrap.append(listOutput)
         movieCoverWrap.append(coverOutput)
       })
