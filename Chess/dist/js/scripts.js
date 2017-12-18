0f5390885961a4077694fcae0cf99891c5c7337a
Dongwoo Lee <likedemian@me.com>
2017-12-18 23:46:19 +0900

    Add: new Projects named Othello

--- Chess/dist/js/scripts.js
+++ Chess/dist/js/scripts.js
@@ -1,3 +1,9 @@
 var black = document.getElementById('black');
 var white = document.getElementById('white');
+var a5 = document.getElementById('a5')
 
+
+
+a5.onclick = function log() {
+  console.log("clicked");
+}
