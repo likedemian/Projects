541b8ab24845e873d83520557110dfaf4257410e
Dongwoo Lee <likedemian@me.com>
2017-12-26 23:57:04 +0900

    Add: Tic Tac Toe

--- tic-tac-toe/dist/js/scripts.js
+++ tic-tac-toe/dist/js/scripts.js
@@ -1,33 +1,63 @@
-var matrixContainer = document.getElementById('matrix__container');
-var display = document.getElementById('display');
-var reset = document.getElementById('reset-btn');
-var count = 0;
-
-
 function board() {
-  display.innerHTML = "O의 차례입니다."
+  var arr = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
+
+  var m1 = document.getElementById('m1');
+  var m2 = document.getElementById('m2');
+  var m3 = document.getElementById('m3');
+  var m4 = document.getElementById('m4');
+  var m5 = document.getElementById('m5');
+  var m6 = document.getElementById('m6');
+  var m7 = document.getElementById('m7');
+  var m8 = document.getElementById('m8');
+  var m9 = document.getElementById('m9');
+
+  arr[0][0] = m1;
+  arr[0][1] = m2;
+  arr[0][2] = m3;
+  arr[1][0] = m4;
+  arr[1][1] = m5;
+  arr[1][2] = m6;
+  arr[2][0] = m7;
+  arr[2][1] = m8;
+  arr[2][2] = m9;
+
+  return arr;
+}
+
+function clickMatrix(arr) {
+  var count = 0;
   for (var i = 0; i < 3; i++) {
     for (var j = 0; j < 3; j++) {
-      var matrix = document.createElement('button');
-      matrixContainer.append(matrix);
-      matrix.classList.add("matrix");
-      matrix.innerHTML = "­­­ ";
-      matrix.id = [i, j];
-      matrix.onclick = function() {
-        if (count % 2 === 0 && this.innerHTML != "X") {
-          this.innerHTML = "O";
-          display.innerHTML = "X의 차례입니다.";
+      arr[i][j].onclick = function() {
+        if (count % 2 === 0 && this.innerText != "X") {
+          this.innerText = "O";
           count++;
-        } else if (count % 2 !== 0 && this.innerHTML != "O") {
-          this.innerHTML = "X"
-          display.innerHTML = "O의 차례입니다.";
+        } else if (count % 2 !== 0 && this.innerText != "O") {
+          this.innerText = "X"
           count++;
         }
       }
     }
   }
-  console.log(matrix.id.split(',')[0]);
+  return arr;
+}
+
+function checkWinner(arr) {
+  if (arr[0][0] == arr[0][1] && arr[0][2]) {
+    console.log(arr[0][0], "이 이김");
+  }
+  return arr;
 }
 
 
-board();
+
+(function() {
+
+  var matrix = board();
+  clickMatrix(matrix);
+  checkWinner(matrix);
+})();
